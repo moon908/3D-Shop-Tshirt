4725a177227b43434f243b0797d03afb5b98e34b
Siddhesh Moon <siddheshmoon908u@gmail.com>
2026-01-04 15:36:22 +0530

    Removing Aipicker

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -8,7 +8,7 @@ import { download } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
 import { fadeAnimation, slideAnimation } from '../config/motion';
-import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
+import { ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
     const snap = useSnapshot(state);
@@ -30,18 +30,7 @@ const Customizer = () => {
             case "colorpicker":
                 return <ColorPicker />
             case "filepicker":
-                return <FilePicker
-                    file={file}
-                    setFile={setFile}
-                    readFile={readFile}
-                />
-            case "aipicker":
-                return <AIPicker
-                    prompt={prompt}
-                    setPrompt={setPrompt}
-                    generatingImg={generatingImg}
-                    handleSubmit={handleSubmit}
-                />
+                return <FilePicker file={file} setFile={setFile} readFile={readFile} />
             default:
                 return null;
         }
